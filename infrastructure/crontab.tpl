0ca50b4cd852477929d8355766afdc8899a2b2a2
Ame <ame180x@gmail.com>
2023-01-20 01:12:15 +0100

    Add envsubst and change crontab file to crontab.tpl that is based on APP_PATH rather than hardcoded path to the application

---
+++ infrastructure/crontab.tpl
@@ -0,0 +1,1 @@
+*/15 * * * * /usr/local/bin/php ${APP_PATH}/bin/drm update_players >> /var/log/cron.log 2>&1
--- infrastructure/entrypoint.sh
+++ infrastructure/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+envsubst < /etc/cron.d/crontab.tpl > /etc/cron.d/crontab
+rm /etc/cron.d/crontab.tpl
+chmod 644 /etc/cron.d/crontab
+touch /var/log/cron.log
+/usr/bin/crontab /etc/cron.d/crontab
 service cron start
 
 apache2-foreground
