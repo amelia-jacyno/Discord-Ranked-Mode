622347e434f00fc75db4c6bc37e9a0c4dbed8441
Ame <ame180x@gmail.com>
2023-01-20 01:35:31 +0100

    Add missing Bootstrap meta tags and change layout from table with ranks to table per rank

--- public/index.php
+++ public/index.php
@@ -14,6 +14,8 @@ $ranks = PlayerRanksResolver::resolvePlayerRanks($players);
 
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
@@ -24,33 +26,36 @@ $ranks = PlayerRanksResolver::resolvePlayerRanks($players);
     <title>Discord Ranked Mode</title>
 </head>
 <body>
-<div class="container">
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Rank</th>
-            <th scope="col">Name</th>
-        </tr>
-        </thead>
-        <tbody>
-		<?php
-		$index = 1;
-		foreach ($ranks as $rank => $players) {
+<div class="container py-3">
+	<?php
+	$index = 1;
+	foreach ($ranks as $rank => $players) {
+		?>
+        <h3 class="text-center"><?= $rank ?></h3>
+        <table class="table table-hover table-fixed mb-4" style="table-layout: fixed">
+            <thead>
+            <tr>
+                <th scope="col" colspan="1">#</th>
+                <th scope="col" colspan="3">Name</th>
+            </tr>
+            </thead>
+            <tbody>
+			<?php
 			foreach ($players as $player) {
 				echo "
-                    <tr>
-                        <th scope=\"row\">$index</th>
-                        <td>$rank</td>
-                        <td>{$player->getUsername()}</td>
-                    </tr>
-                    ";
+                        <tr>
+                            <td colspan='1'>$index</td>
+                            <td colspan='3'>{$player->getUsername()}</td>
+                        </tr>
+                        ";
 				$index++;
 			}
-		}
-		?>
-        </tbody>
-    </table>
+			?>
+            </tbody>
+        </table>
+		<?php
+	}
+	?>
 </div>
 </body>
 </html>
