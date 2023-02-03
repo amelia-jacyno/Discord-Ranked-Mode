b7a9d124b9683e158b95d1a951d6c8d889a8c1ef
Ame <ame180x@gmail.com>
2023-02-03 14:36:44 +0900

    Make volumes and working_dir in docker-compose.yaml also depend on .env APP_PATH value instead of hardcoded value

--- infrastructure/entrypoint.sh
+++ infrastructure/entrypoint.sh
@@ -3,7 +3,7 @@
 envsubst < /etc/cron.d/crontab.tpl > /etc/cron.d/crontab
 rm /etc/cron.d/crontab.tpl
 chmod 644 /etc/cron.d/crontab
-touch /var/log/cron.log
+touch log/cron.log
 /usr/bin/crontab /etc/cron.d/crontab
 service cron start
 
