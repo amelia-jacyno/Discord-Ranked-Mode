9abb292dbb75d4b2e583a9aa003182892eee6b2e
Ame <ame180x@gmail.com>
2023-02-09 17:22:02 +0900

    Fix EntityManagerProvider deprecation

--- src/Service/EntityManagerProvider.php
+++ src/Service/EntityManagerProvider.php
@@ -2,22 +2,27 @@
 
 namespace App\Service;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\MissingMappingDriverImplementation;
 use Doctrine\ORM\ORMSetup;
 
 final class EntityManagerProvider
 {
     private static EntityManager $entityManager;
 
-    /**
-     * @throws ORMException
-     */
-    public static function getEntityManager(): EntityManager
+	/**
+	 * @throws MissingMappingDriverImplementation
+	 * @throws Exception
+	 */
+	public static function getEntityManager(): EntityManagerInterface
     {
         if (!isset(self::$entityManager)) {
-            self::$entityManager = EntityManager::create(self::getConnection(), self::getConfig());
+            self::$entityManager = new EntityManager(self::getConnection(), self::getConfig());
         }
 
         return self::$entityManager;
@@ -31,11 +36,14 @@ final class EntityManagerProvider
         );
     }
 
-    private static function getConnection(): array
+	/**
+	 * @throws Exception
+	 */
+	private static function getConnection(): Connection
     {
-        return [
+        return DriverManager::getConnection([
             'driver' => 'pdo_sqlite',
             'path' => __ROOT__ . '/' . $_ENV['DB_PATH'],
-        ];
+        ], self::getConfig());
     }
 }
