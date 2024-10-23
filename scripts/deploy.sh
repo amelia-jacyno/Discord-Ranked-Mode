6baffedfbaba11f83f1091d050876db5b388ee6d
Ame <ame180x@gmail.com>
2024-10-23 05:24:56 +0200

    Update deploy.sh to not continue when a step fails

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 source .env
 
-scp compose-prod.yaml $SSH_USER@$SSH_HOST:$SSH_PATH/compose.yaml
-ssh $SSH_USER@$SSH_HOST "cd $SSH_PATH && docker compose pull && docker compose up -d --force-recreate"
+scp compose-prod.yaml $SSH_USER@$SSH_HOST:$SSH_PATH/compose.yaml \
+  && ssh $SSH_USER@$SSH_HOST "cd $SSH_PATH && docker compose pull && docker compose up -d --force-recreate"
