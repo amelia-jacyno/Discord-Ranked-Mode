8131630be9e627c51544b30f4737d6c9649e117e
Ame <ame180x@gmail.com>
2024-10-25 01:59:14 +0200

    Reconfigure the app infrastructure to use nginx and php-fpm, use an anonymous volume to share assets from php container to nginx, update php to 8.3 and update dependencies

--- infrastructure/entrypoint-prod.sh
+++ infrastructure/entrypoint-prod.sh
@@ -2,9 +2,10 @@
 
 mkdir cache
 chown -R www-data:www-data cache
-/usr/bin/crontab /etc/cron.d/crontab
-service cron start
 
 vendor/bin/phinx migrate
 
-apache2-foreground
+/usr/bin/crontab /etc/cron.d/crontab
+service cron start
+
+php-fpm
