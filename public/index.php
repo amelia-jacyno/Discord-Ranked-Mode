ad9acd80b03e09f1b77ef28c6a693e6877f94dc8
Ame <ame180x@gmail.com>
2024-10-25 04:11:02 +0200

    Remove routes.php and move logic back to index.php for now

--- public/index.php
+++ public/index.php
@@ -5,10 +5,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+use App\Controller\LeaderboardController;
+use HaydenPierce\ClassFinder\ClassFinder;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 require_once '../bootstrap.php';
 
-$routes = require_once '../src/routes.php';
+$routes = new RouteCollection();
+$controllers = ClassFinder::getClassesInNamespace('App\Controller');
+foreach ($controllers as $controller)
+{
+    $class = new ReflectionClass($controller);
+    $methods = $class->getMethods();
+
+    foreach ($methods as $method)
+    {
+        $routeAttribute = $method->getAttributes(\Symfony\Component\Routing\Attribute\Route::class);
+        foreach ($routeAttribute as $attribute)
+        {
+            /** @var \Symfony\Component\Routing\Attribute\Route $route */
+            $route = $attribute->newInstance();
+            $routes->add($route->getName(), new Route($route->getPath(), [
+                '_controller' => [LeaderboardController::class, $method->getName()],
+            ]));
+        }
+    }
+}
+
 $request = Request::createFromGlobals();
 
 $context = new RequestContext();
