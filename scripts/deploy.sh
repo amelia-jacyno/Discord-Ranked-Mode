2d6094d87634c878ee2282987bd5d58c303e8e5e
Ame <ame180x@gmail.com>
2024-10-25 02:17:07 +0200

    Reconfigure the app infrastructure to use nginx and php-fpm, use an anonymous volume to share assets from php container to nginx, update php to 8.3 and update dependencies

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,12 @@
 
 source .env
 
+SSH_APP_PREFIX=${SSH_APP_PREFIX:-${SSH_PATH}}
+
 scp compose-prod.yaml $SSH_USER@$SSH_HOST:$SSH_PATH/compose.yaml \
-  && ssh $SSH_USER@$SSH_HOST "cd $SSH_PATH && docker compose pull && docker compose up -d --force-recreate"
+  && ssh $SSH_USER@$SSH_HOST "cd $SSH_PATH \
+    && docker compose pull \
+    && docker compose down \
+    && (docker volume rm ${SSH_APP_PREFIX}_assets || true) \
+    && docker compose up -d --force-recreate
+  "
