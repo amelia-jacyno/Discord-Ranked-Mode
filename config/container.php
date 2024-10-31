f4a6f6a429f272afcd6c0b2f31f55332b7a341e4
Ame <ame180x@gmail.com>
2024-10-31 04:24:57 +0100

    Move twig container configuration into packages/twig.php from container.php

--- config/container.php
+++ config/container.php
@@ -17,14 +17,6 @@ $builder->addDefinitions([
             isDevMode: 'dev' === $_ENV['APP_ENV'],
         );
     },
-    Twig\Environment::class => function () {
-        $twigLoader = new Twig\Loader\FilesystemLoader(__ROOT__ . '/src/View');
-        $twigCache = 'prod' === $_ENV['APP_ENV'] ? __ROOT__ . '/cache/twig' : false;
-
-        return new Twig\Environment($twigLoader, [
-            'cache' => $twigCache,
-        ]);
-    },
     'App\Controller\*' => DI\autowire()
         ->method('setContainer', DI\get(Psr\Container\ContainerInterface::class)),
     Psr\Container\ContainerInterface::class => DI\get(DI\Container::class),
---
+++ config/packages/twig.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'twig.paths' => [
+        __ROOT__ . '/src/View',
+    ],
+    'twig.options' => [
+        'cache' => 'prod' === $_ENV['APP_ENV'] ? __ROOT__ . '/cache/twig' : false,
+    ],
+    'twig.loader' => DI\get(\Twig\Loader\LoaderInterface::class),
+    \Twig\Loader\LoaderInterface::class => DI\get(\Twig\Loader\FilesystemLoader::class),
+    \Twig\Loader\FilesystemLoader::class => DI\create()
+        ->constructor(DI\get('twig.paths')),
+    Twig\Environment::class => DI\create()
+        ->constructor(DI\get('twig.loader'), DI\get('twig.options')),
+];
