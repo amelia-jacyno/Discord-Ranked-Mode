157a84987cc741eb4df174de8c43f4aea4dfb8a4
Ame <ame180x@gmail.com>
2024-10-31 04:38:13 +0100

    Fix style

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -7,9 +7,9 @@ return [
     'twig.options' => [
         'cache' => 'prod' === $_ENV['APP_ENV'] ? __ROOT__ . '/cache/twig' : false,
     ],
-    'twig.loader' => DI\get(\Twig\Loader\LoaderInterface::class),
-    \Twig\Loader\LoaderInterface::class => DI\get(\Twig\Loader\FilesystemLoader::class),
-    \Twig\Loader\FilesystemLoader::class => DI\create()
+    'twig.loader' => DI\get(Twig\Loader\LoaderInterface::class),
+    Twig\Loader\LoaderInterface::class => DI\get(Twig\Loader\FilesystemLoader::class),
+    Twig\Loader\FilesystemLoader::class => DI\create()
         ->constructor(DI\get('twig.paths')),
     Twig\Environment::class => DI\create()
         ->constructor(DI\get('twig.loader'), DI\get('twig.options')),
