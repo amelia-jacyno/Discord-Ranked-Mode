79b476005c3b250d96215c756fe4b99624316f56
Ame <ame180x@gmail.com>
2024-11-07 07:31:12 +0100

    Fix style

--- config/container.php
+++ config/container.php
@@ -3,7 +3,7 @@
 $builder = new DI\ContainerBuilder();
 $builder->addDefinitions([
     'env' => $_ENV['APP_ENV'] ?? 'prod',
-    'debug' =>  fn (DI\Container $c) => $c->get('env') === 'dev',
+    'debug' => fn (DI\Container $c) => 'dev' === $c->get('env'),
     Psr\Container\ContainerInterface::class => DI\get(DI\Container::class),
     'App\Controller\*' => DI\autowire()
         ->method('setContainer', DI\get(Psr\Container\ContainerInterface::class)),
